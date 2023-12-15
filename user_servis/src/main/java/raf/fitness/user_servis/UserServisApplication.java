73304d34601379b1d0ce5f3003789af9eee9afc5
Irina Tomic <irina.z.tomic@gmail.com>
2023-12-15 15:22:59 +0100

    Add user_servis spring app

---
+++ user_servis/src/main/java/raf/fitness/user_servis/UserServisApplication.java
@@ -0,0 +1,13 @@
+package raf.fitness.user_servis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServisApplication.class, args);
+	}
+
+}
---
+++ user_servis/src/test/java/raf/fitness/user_servis/UserServisApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.fitness.user_servis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
