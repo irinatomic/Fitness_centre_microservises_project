8a356d320f937e7e9c40ca3db42a5836d85c6bfe
kvucicevic5721rn <kvucicevic5721rn@raf.rs>
2023-12-15 20:59:55 +0100

    Added security and implemented tokenDTO

--- user_servis/src/main/java/raf/fitness/user_servis/dto/token/TokenResponseDto.java
+++ user_servis/src/main/java/raf/fitness/user_servis/dto/token/TokenResponseDto.java
@@ -1,4 +1,20 @@
 package raf.fitness.user_servis.dto.token;
 
 public class TokenResponseDto {
+    private String token;
+
+    public TokenResponseDto() {
+    }
+
+    public TokenResponseDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
