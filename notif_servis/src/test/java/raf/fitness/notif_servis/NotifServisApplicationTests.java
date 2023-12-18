ec6edb0f7e092ff52a8f14c8f403b13b196f71a0
Irina Tomic <irina.z.tomic@gmail.com>
2023-12-18 20:41:47 +0100

    Init Java Spring app

---
+++ notif_servis/src/test/java/raf/fitness/notif_servis/NotifServisApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.fitness.notif_servis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifServisApplicationTests {
+
+	 @Test
+	void contextLoads() {
+	}
+
+}
